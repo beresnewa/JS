a2c9510bd3a0798dc7ae3c9c027c90e6ad039a1f
beresnewa <ekaretinaberesneva5@gmail.com>
2020-03-12 10:45:09 +0300

    get

--- main.js
+++ main.js
@@ -1,22 +1,55 @@
+function Car (brand, model, type, cc) {
+    
+    var _brand = brand;
+    this.getBrand = function (){
+        return _brand;
+    }
 
-for (let i=0; i<10; i++){
-    setTimeout (function (){
-        console.log (i)
-    }, 1000*i, i);
-};
 
-function a (seconds){
-    console.log ('¯\\_(ツ)_/¯    ' , seconds);
-    setTimeout (a, seconds*1000, seconds + 1);
-}
+    var _model = model;
+    this.getModel = function (){
+        return _model;
+    }
 
-a (1);
+    var _type = type;
+    this.getType = function (){
+        return _type;
+    }
 
-function factorial (n) {
-    if (n>1){
-        return n * factorial(n-1);
-    } else {
-        return n;
+    var _cc = cc;
+    this.getCC = function (){
+        return _cc;
     }
 };
- console.log (factorial (6));
+
+
+let car1 = new Car('Ford', 'Mustang', 'Roadster', 5000);
+let car2 = new Car('Renault', 'Logan', 'Sedan', 2000);
+let car3 = new Car('BMW', 'X6', 'Roadster', 7000);
+let car4 = new Car('Volkswagen', 'Polo', 'Sedan', 2800);
+let car5 = new Car('Audi', 'A8', 'Coupe', 4000);
+
+const cars = [car1, car2, car3, car4, car5];
+
+
+
+function stringOfCarModel () {
+    console.log(`${this.getBrand()} ${this.getModel()} is ${this.getType()} and can do wroom-wroom ${this.getCC()} cc`);
+    return
+};
+
+function checkCC (cars) {
+    cars.forEach((car) => {
+        
+        if (car.getCC() > 3000){
+        // console.log (car.getCC());
+           stringOfCarModel.call(car);
+        }
+       
+    });
+
+};
+
+checkCC (cars);
+
+
